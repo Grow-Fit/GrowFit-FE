9dd14775d4a49d02cb4de4343d3cb6187d16ed01
gayoung <did-619@naver.com>
2025-04-04 00:22:37 +0900

    🔧: scss 전역스타일을 위한 설정 #4

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  sassOptions: {
+    includePaths: [path.join(__dirname, "src/styles")],
+    prependData: `@use "variables" as *;`, // 모든 SCSS 파일에서 자동 적용
+  },
 };
 
 export default nextConfig;
