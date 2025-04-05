551272f9dca6789d5db791faa588ed36a4bfa953
gayoung <did-619@naver.com>
2025-04-06 01:19:54 +0900

    ♻️ : 파일분리에 따른 설정파일 수정  #4

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,10 @@ import path from "path";
 const nextConfig: NextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "src/styles")],
-    prependData: `@use "variables" as *;`, // 모든 SCSS 파일에서 자동 적용
+    prependData: `
+      @use "colors" as *;
+      @use "typography" as *;
+    `,
   },
 };
-
 export default nextConfig;
