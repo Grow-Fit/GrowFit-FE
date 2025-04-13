a1ef75395a37eb05863d5742444dd1c388e364a2
gayoung <did-619@naver.com>
2025-04-14 00:26:35 +0900

    ✨ : 추가/추가완료 스토리 컴포넌트 제작 #17

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,22 +1,35 @@
-import type { StorybookConfig } from "@storybook/experimental-nextjs-vite";
-
-const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+import path from 'path';
+import {mergeConfig} from "vite";
+const config = {
+  stories: [
+    '../src/**/*.mdx',
+    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
-  "addons": [
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test"
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@chromatic-com/storybook',
+    '@storybook/experimental-addon-test',
   ],
-  "framework": {
-    "name": "@storybook/experimental-nextjs-vite",
-    "options": {}
+  framework: {
+    name: '@storybook/experimental-nextjs-vite',
+    options: {},
   },
-  "staticDirs": [
-    "../public"
-  ]
+  staticDirs: ['../public'],
+  viteFinal(config) {
+    return mergeConfig(config, {
+      css: {
+        preprocessorOptions: {
+          scss: {
+            additionalData: `
+              @use "/src/styles/colors" as *;
+              @use "/src/styles/typography" as *;
+            `,
+          },
+        },
+      },
+    });
+  },
 };
+
 export default config;
