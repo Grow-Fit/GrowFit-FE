d1b566187a41d882bff7c885461c28c56fece4ac
hyezung <93978777+hyezung@users.noreply.github.com>
2025-04-13 11:26:36 -0400

    ✨ : svg 아이콘 추가

--- next.config.ts
+++ next.config.ts
@@ -9,5 +9,16 @@ const nextConfig: NextConfig = {
       @use "typography" as *;
     `,
   },
-};
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+        },
+      ],
+    })
+    return config
+  },
+}
 export default nextConfig;
