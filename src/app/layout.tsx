1815a0bc0201d49bbd7aafab664233a46d4dde86
gayoung <did-619@naver.com>
2025-04-26 18:56:54 +0900

    ✨ : 전역 레이아웃 스타일 추가 #22

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local"
 import QueryProvider from "@/queries/QueryProvider"
 
 import "@/styles/globals.scss"
+import Layout from "@/components/layout/Layout";
 
 const Pretendard = localFont({
   src: "../styles/fonts/PretendardVariable.woff2",
@@ -16,7 +17,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={Pretendard.className}>
-        <QueryProvider>{children}</QueryProvider>
+        <Layout>
+          <QueryProvider>{children}</QueryProvider>
+        </Layout>
       </body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import Logo from "@/assets/logo/growfit-logo.svg"
-import Button from "@/components/common/button"
 
 export default function Home() {
   return (
     <div>
-      <Button>Click me!</Button>
       <Logo />
     </div>
   )
