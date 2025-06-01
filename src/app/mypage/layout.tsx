a1d14e787691a89d3f951a1319d3792dd1c8cfd7
gayoung <did-619@naver.com>
2025-06-01 19:02:55 +0900

    ✨: 마이페이지 수정 레이아웃 완료

--- src/app/mypage/layout.tsx
+++ src/app/mypage/layout.tsx
@@ -1,19 +1,8 @@
 "use client"
-import React from "react";
-import DefaultHeader from "@/components/layout/header/DefaultHeader";
-import Navigation from "@/components/layout/Navigation";
+import React from "react"
+import Navigation from "@/components/layout/Navigation"
 import styles from "./layout.module.scss"
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <>
-      <DefaultHeader/>
-      <div>
-        <main className={styles.container}>
-          {children}
-        </main>
-        <Navigation/>
-      </div>
-    </>
-  )
+  return <main className={styles.container}>{children}</main>
 }
--- vitest.config.ts
+++ vitest.config.ts
@@ -3,11 +3,10 @@ import path from "node:path"
 import { fileURLToPath } from "node:url"
 import { defineConfig } from "vitest/config"
 
-
 const dirname =
   typeof __dirname !== "undefined" ? __dirname : path.dirname(fileURLToPath(import.meta.url))
 
-// More info at: https://storybook.js.org/docs/writing-tests/test-addon
+// More detail at: https://storybook.js.org/docs/writing-tests/test-addon
 export default defineConfig({
   test: {
     workspace: [
