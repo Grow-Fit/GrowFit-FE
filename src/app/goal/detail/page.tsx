7b0770b839fa44576cde210eab6b216c1569d540
gayoung <did-619@naver.com>
2025-06-04 15:48:09 +0900

    ♻️: goal 페이지 레이아웃 공통 scss로 변경

--- src/app/goal/detail/page.tsx
+++ src/app/goal/detail/page.tsx
@@ -6,38 +6,40 @@ import SmallCalendar from "@/components/common/smallcalendar/smallcalendar"
 import StepBox from "@/components/common/stepbox/stepbox"
 import { useRouter } from "next/navigation"
 import TopSheet from "@/components/common/topsheet/topsheet"
+import DefaultHeader from "@/components/layout/header/DefaultHeader"
+import Navigation from "@/components/layout/Navigation"
 
 const Page = () => {
   const router = useRouter()
   const openModal = () => {
-    router.push("/goal/detail/letter-modal")
+    router.push("/goal/detail/goal-modal")
   }
   return (
-    <>
-      <div className={styles.goal}>
-        <div className={styles.goal__content}>
-          <div className={styles.goal__calendar}>
-            <TopSheet>
-              <SmallCalendar />
-            </TopSheet>
-          </div>
+    <main className={styles.goal}>
+      <DefaultHeader />
+      <div className={styles.goal__content}>
+        <div className={styles.goal__calendar}>
+          <TopSheet>
+            <SmallCalendar />
+          </TopSheet>
+        </div>
 
-          <div className={styles.goal__character}>
-            <Character1 />
-          </div>
+        <div className={styles.goal__character}>
+          <Character1 />
+        </div>
 
-          <div className={styles.goal__profile} onClick={openModal}>
-            <span>1월 24일 ~ 1월 31일 목표</span>
-            <span>한 주 동안 레벨 업! 더 가볍고 건강하게!</span>
-            <p>0/5개 달성</p>
-          </div>
+        <div className={styles.goal__profile} onClick={openModal}>
+          <span>1월 24일 ~ 1월 31일 목표</span>
+          <span>한 주 동안 레벨 업! 더 가볍고 건강하게!</span>
+          <p>0/5개 달성</p>
+        </div>
 
-          <div className={styles.goal__box}>
-            <StepBox />
-          </div>
+        <div className={styles.goal__box}>
+          <StepBox />
         </div>
       </div>
-    </>
+      <Navigation />
+    </main>
   )
 }
 
