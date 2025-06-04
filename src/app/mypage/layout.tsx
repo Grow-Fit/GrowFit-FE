21622d681720b0a44159ecb800d59f13c87e541c
gayoung <did-619@naver.com>
2025-06-04 15:52:31 +0900

    ♻️: 마이페이지 레이아웃 공통 scss로 변경

--- src/app/mypage/layout.tsx
+++ src/app/mypage/layout.tsx
@@ -1,8 +1,6 @@
-"use client"
 import React from "react"
-import Navigation from "@/components/layout/Navigation"
 import styles from "./layout.module.scss"
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <main className={styles.container}>{children}</main>
+  return <div className={styles.container}>{children}</div>
 }
