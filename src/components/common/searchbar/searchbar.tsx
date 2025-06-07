8f787465f476cbc79836e4493b93fff1eaecb482
gayoung <did-619@naver.com>
2025-06-07 21:05:07 +0900

    ✨: 검색컴포넌트 추가

---
+++ src/components/common/searchbar/searchbar.tsx
@@ -0,0 +1,25 @@
+"use client"
+import styles from "./searchbar.module.scss"
+import { DeleteIcon, SearchIcon } from "@/components/common/icon"
+import { useState } from "react"
+
+const SearchBar = ({ placeholder }) => {
+  const [value, setValue] = useState("")
+
+  const handleChange = (e) => {
+    setValue(e.target.value)
+  }
+
+  const clearInput = () => {
+    setValue("")
+  }
+  return (
+    <div className={styles.searchbar}>
+      <SearchIcon />
+      <input placeholder={placeholder} value={value} onChange={handleChange} />
+      {value && <DeleteIcon onClick={clearInput} />}
+    </div>
+  )
+}
+
+export default SearchBar
