d310e5654b9ea324e8108ba41dc4fbbb6d2696fa
sukyung6999 <sukyung6999@gmail.com>
2025-06-08 23:52:51 +0900

    ✨: 아이 로그인 zustand 상태 관리 #52

---
+++ src/app/(auth)/join/[type]/[step]/_components/ChildStep01.tsx
@@ -0,0 +1,60 @@
+"use client"
+
+import { useState } from "react"
+import Link from "next/link"
+import { useRouter } from "next/navigation"
+
+import CameraIcon from "@/assets/icons/common/icon-24-camera.svg"
+import Button from "@/components/common/button/button"
+import Input from "@/components/common/input/input"
+import { useUserStore } from "@/stores/userStore"
+
+import pageStyles from "../page.module.scss"
+import styles from "./steps.module.scss"
+
+const ChildStep01 = () => {
+  const router = useRouter()
+  const { updateChild } = useUserStore()
+  const [qrValue, setQrValue] = useState("")
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQrValue(e.target.value)
+  }
+  const handleClickNext = () => {
+    updateChild({
+      parentQRCode: qrValue,
+    })
+    router.push("/join/child/2")
+  }
+  return (
+    <>
+      <div className={styles.box__step1}>
+        <Input
+          id="qrCode"
+          type="text"
+          inputSize="w-full"
+          variant="withoutLabel"
+          shape="border"
+          label="큐알코드"
+          placeholder="코드 입력"
+          onChange={handleChange}
+          value={qrValue}
+        />
+        <button type="button" className={styles.btn_qr}>
+          QR 스캔하기
+          <CameraIcon />
+        </button>
+      </div>
+      <Button
+        label="다음"
+        shape="rounded"
+        size="large"
+        variant="filled"
+        classNames={pageStyles.join__content__btn}
+        onClick={handleClickNext}
+        aria-disabled={qrValue === ""}
+      />
+    </>
+  )
+}
+export default ChildStep01
