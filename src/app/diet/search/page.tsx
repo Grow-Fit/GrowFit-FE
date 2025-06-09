a962e9562a4a3fc7a6fdccf6077f0567589ee84c
gayoung <did-619@naver.com>
2025-06-09 12:15:26 +0900

    ✨: 영양소 검색 데이터 레이아웃

--- src/app/diet/search/page.tsx
+++ src/app/diet/search/page.tsx
@@ -5,6 +5,7 @@ import SearchBar from "@/components/common/searchbar/searchbar"
 import { NodataIcon } from "@/components/common/icon"
 import { AddIcon } from "@/components/common/icon"
 import Link from "next/link"
+import AddButton from "@/components/common/addbutton/addbutton"
 
 const Page = () => {
   const onSubmit = () => {
@@ -23,6 +24,25 @@ const Page = () => {
           <p>영양소 직접 추가하기</p>
           <AddIcon />
         </Link>
+      </div>
+      <div className={styles.search__result}>
+        <div className={styles["search__result-item"]}>
+          <div className={styles["search__result-text"]}>
+            <h3>닭가슴살 샐러드</h3>
+            <p>109kcal</p>
+          </div>
+          <div className={styles["search__result-icon"]}>
+            <AddButton disabled={false} onClick={() => {}} />
+          </div>
+        </div>
+      </div>
+      <div className={styles.search__btn}>
+        <p>
+          <span>아침메뉴</span>에
+        </p>
+        <button>
+          <span>1개</span> 기록하기
+        </button>
       </div>
     </div>
   )
