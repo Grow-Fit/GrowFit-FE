714c8325460e2aa19541b356390163845941dab9
hyezung <93978777+hyezung@users.noreply.github.com>
2025-06-16 19:01:36 -0400

    qr페이지 생성
    qr페이지 생성 및 홈화면 레이아웃 수정

--- src/components/common/goalgauge/goalgauge.tsx
+++ src/components/common/goalgauge/goalgauge.tsx
@@ -1,16 +1,18 @@
 
-import Gauge0 from "@/assets/icons/gauge bars/calorie.svg";
-import Gauge1 from "@/assets/icons/gauge bars/gauge bar.svg";
-import Gauge2 from "@/assets/icons/gauge bars/gauge bar-1.svg";
-import Gauge3 from "@/assets/icons/gauge bars/gauge bar-2.svg";
-import Gauge4 from "@/assets/icons/gauge bars/gauge bar-3.svg";
-import Gauge5 from "@/assets/icons/gauge bars/gauge bar-4.svg";
-import Gauge6 from "@/assets/icons/gauge bars/gauge bar-5.svg";
-import Gauge7 from "@/assets/icons/gauge bars/gauge bar-6.svg";
-import Gauge8 from "@/assets/icons/gauge bars/gauge bar-7.svg";
-import Gauge9 from "@/assets/icons/gauge bars/gauge bar-8.svg";
+import Gauge0 from "@/assets/icons/gauge bars/gauage_bar/gauge bar.svg"
+import Gauge1 from "@/assets/icons/gauge bars/gauage_bar/gauge bar-1.svg"
+import Gauge2 from "@/assets/icons/gauge bars/gauage_bar/gauge bar-2.svg"
+import Gauge3 from "@/assets/icons/gauge bars/gauage_bar/gauge bar-3.svg"
+import Gauge4 from "@/assets/icons/gauge bars/gauage_bar/gauge bar-4.svg"
+import Gauge5 from "@/assets/icons/gauge bars/gauage_bar/gauge bar-5.svg"
+import Gauge6 from "@/assets/icons/gauge bars/gauage_bar/gauge bar-6.svg"
+import Gauge7 from "@/assets/icons/gauge bars/gauage_bar/gauge bar-7.svg"
+import Gauge8 from "@/assets/icons/gauge bars/gauage_bar/gauge bar-8.svg"
+import Gauge9 from "@/assets/icons/gauge bars/gauage_bar/gauge bar-9.svg"
+import Gauge10 from "@/assets/icons/gauge bars/gauage_bar/gauge bar-10.svg"
+import Gauge11 from "@/assets/icons/gauge bars/gauage_bar/gauge bar-11.svg"
 
-export type GaugeStep = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+export type GaugeStep = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11
 
 interface Props {
   step: GaugeStep;
@@ -27,7 +29,9 @@ const gaugeMap: Record<GaugeStep, React.FC<React.SVGProps<SVGSVGElement>>> = {
   6: Gauge6,
   7: Gauge7,
   8: Gauge8,
-  9: Gauge9
+  9: Gauge9,
+  10: Gauge10,
+  11: Gauge11
 };
 
 export default function GaugeIcon({ step, className }: Props) {
